e87b0b925101016667aa2f81d1bf683af99c26cd
Max McCready <msmccready@gmail.com>
2022-07-10 22:34:27 -0600

    almost there on dates

--- texts.py
+++ texts.py
@@ -1,8 +1,10 @@
 from time import time
+import math
 from tracemalloc import start
 from twilio.rest import Client
 from datetime import datetime, timedelta
-import pytz
+from delorean import Delorean, epoch
+import calendar
 
 def schedule(show):
     account_sid = "ACfe19105a3aa7d11c16d6272a0d3eccda"
@@ -10,20 +12,14 @@ def schedule(show):
     client = Client(account_sid, auth_token)
     station = show[2]
 
-    # get show datetime
-    # convert day to nearest future day with matching weekday
-    # enter in time manually
+    # get now in the timezone
+    # get scheduled show in the timezone
+    # convert scheduled show to utc
     show_day = show[3]
     start_time = int(show[4])
     next_show_date = (datetime.utcnow() + timedelta(days=get_day_difference(show_day))).date()
     exact = datetime(year=next_show_date.year, month=next_show_date.month, day=next_show_date.day, hour=start_time)
     print(exact)
-    
-    # convert to UTC
-
-
-    # time_utc = convert_to_utc('US/Central')
-    # print("time utc: ", time_utc)
 
     # message = client.messages.create(
     #      messaging_service_sid='MG73e4d89da9b2863a263e62abccc879a1',
@@ -35,35 +31,71 @@ def schedule(show):
 
     # print(message.sid)
 
-def get_day_difference(show_weekday = 3):
-    today_weekday = datetime.utcnow().weekday()
-    print(today_weekday)
-    if(today_weekday == show_weekday):
-        return 0
-    # if later in the week
-    elif(show_weekday > today_weekday):
-        return show_weekday - today_weekday
-    # if earlier in next week
-    elif(show_weekday < today_weekday):
-        return (show_weekday + 1 + (6-today_weekday))
+def now_or_later(show_day, start_time, timezone):
+    # the all-important now
+    nau = Delorean()
+    nau = nau.epoch
 
-# if today is 2 but show is on 4, return 2
-# show is greater than 4 so subtract today from show
-# if today is 5 but show is 1, return show_weekday plus 1 plus remaining
+    def find_next_day(d, show_day, start_hour, start_minute):
+        match show_day:
+            case 0:
+                return d.next_monday().replace(hour=start_hour, minute=start_minute).truncate('minute')
+            case 1:
+                return d.next_tuesday().replace(hour=start_hour, minute=start_minute).truncate('minute')
+            case 2:
+                return d.next_wednesday().replace(hour=start_hour, minute=start_minute).truncate('minute')
+            case 3:
+                return d.next_thursday().replace(hour=start_hour, minute=start_minute).truncate('minute')
+            case 4:
+                return d.next_friday().replace(hour=start_hour, minute=start_minute).truncate('minute')
+            case 5:
+                return d.next_saturday().replace(hour=start_hour, minute=start_minute).truncate('minute')
+            case 6:
+                return d.next_sunday().replace(hour=start_hour, minute=start_minute).truncate('minute')
 
+            # If an exact match is not confirmed, this last case will be used if provided
+            case _:
+                return "Something went wrong"
 
-def convert_to_utc(show_datetime, show_timezone):
-    local = pytz.timezone(timezone)
-    local_dt = local.localize(datetime.now(), is_dst=None)
-    utc_dt = local_dt.astimezone(pytz.utc)
-    return utc_dt
-    print(local_dt)
-    print(utc_dt)
+    # when the show will occur
+    then = Delorean().shift(timezone)
+    print("nau", nau)
+    print("then", then)
+    start_hour, start_minute = int(math.modf(start_time)[1]), 30 if int(math.modf(start_time)[0]) == 5 else 0
+    then = find_next_day(then, show_day, start_hour, start_minute)
+    then = then.shift('utc').epoch
+
+    difference = then - nau
+    print("difference", difference)
+    # if more than 15 mins in future and less than one week
+    if(900 < difference < 604800):
+        print('schedule regular')
+    # if greater than one week, shift back then send
+    if(604800 < difference < (604800 * 2)):
+        print('shift back a week then schedule')
+    # if less than 15 minutes in future, send text now!
+    if(difference < 900):
+        print('send now!')
+
 
 def main():
     print(datetime.now() + timedelta(hours=2))
 
 if __name__=="__main__":
-    start_time =  datetime.time(6, 42)
-    print(start_time)
+    now_or_later(6, 22.5, 'US/Mountain')
+    
 
+# def get_day_difference(show_weekday = 3):
+#     today_weekday = datetime.utcnow().weekday()
+#     print(today_weekday)
+#     if(today_weekday == show_weekday):
+#         return 0
+#     # if later in the week
+#     elif(show_weekday > today_weekday):
+#         return show_weekday - today_weekday
+#     # if earlier in next week
+#     elif(show_weekday < today_weekday):
+#         return (show_weekday + 1 + (6-today_weekday))
+
+    
+#     day = list(calendar.day_name)[show_day]
